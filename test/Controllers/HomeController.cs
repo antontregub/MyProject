e97de7d0055ff735db599b69d8c062f75e225aec
antontregub <esesoves@gmail.com>
2018-12-07 00:04:32 +0200

    test

---
+++ test/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using test.Models;
+using PagedList;
+
+namespace test.Controllers
+{
+    public class HomeController : Controller
+    {
+        TenderContext db = new TenderContext();
+        // private SqlConnection sql;
+ 
+         public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+         {
+
+             ViewBag.CurrentSort = sortOrder;
+             // ViewBag.CategorySortParm = String.IsNullOrEmpty(sortOrder) ? "Category" : "";
+             ViewBag.CategorySortParm = sortOrder == "Category" ? "Sum" : "Category";
+             ViewBag.DateSortParm = sortOrder == "Date" ? "Category" : "Date";
+             ViewBag.SumSortPar = sortOrder == "Sum" ? "Category" : "Sum";
+             if (searchString != null)
+             {
+                 page = 1;
+             }
+             else
+             {
+                 searchString = currentFilter;
+             }
+
+             ViewBag.CurrentFilter = searchString;
+
+             var tend = from s in db.Tenders
+                            select s;
+            // ViewBag.position = searchString;
+             if (!String.IsNullOrEmpty(searchString))
+             {
+                 tend = tend.Where(s => s.tender.Contains(searchString)
+                                        || s.description.Contains(searchString));
+             }
+             switch (sortOrder)
+             {
+                   case "Category":
+                     tend = tend.OrderByDescending(s => s.category);
+                   break;
+                 case "Date":
+                     tend = tend.OrderBy(s => s.date);
+                     break;
+                 case "Sum":
+                     tend = tend.OrderBy(s => s.initial_rate);
+                     break;
+                 default:
+                     tend = tend.OrderBy(s => s.tender);
+                     break;
+             }
+             int pageSize = 2;
+             int pageNumber = (page ?? 1);
+             return View(tend.ToPagedList(pageNumber, pageSize));
+         }
+
+        public ActionResult TenderPage(int itemId)
+        {
+            var Item = db.Tenders.FirstOrDefault(x => x.Id == itemId);
+            return View(Item);
+        }
+
+    }
+}
---
+++ test/Models/Tender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace test.Models
+{
+    public partial class Tender
+    {
+        public int Id { get; set; }
+        public string tender { get; set; }
+        public string description { get; set; }
+        public string oganizator { get; set; }
+        public string view { get; set; }
+        public string category { get; set; }
+        public decimal initial_rate { get; set; }
+        public string currency { get; set; }
+        public DateTime date { get; set; }
+        public DateTime date_start { get; set; }
+        public DateTime date_finish { get; set; }
+
+    }
+
+    public class TenderContext : DbContext
+    {
+
+        public DbSet<Tender> Tenders { get; set; }
+
+    }
+    public partial class Tend
+    {
+        public IEnumerable<Tender> Tenders { get; set; }
+        public SelectList Oganizators { get; set; }
+       
+    }
+
+ 
+
+}
